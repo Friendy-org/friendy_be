958114792929dd7db6910bc72a1c0af85456d521
김재원 <rlawodnjs353@naver.com>
2025-02-13 23:07:26 +0900

    test(#54): 멤버fixture 수정

--- src/test/java/friendy/community/domain/member/fixture/MemberFixture.java
+++ src/test/java/friendy/community/domain/member/fixture/MemberFixture.java
@@ -1,8 +1,10 @@
 package friendy.community.domain.member.fixture;
 
+import friendy.community.domain.member.dto.request.MemberSignUpRequest;
 import friendy.community.domain.member.encryption.PasswordEncryptor;
 import friendy.community.domain.member.encryption.SHA2PasswordEncryptor;
 import friendy.community.domain.member.model.Member;
+import org.springframework.mock.web.MockMultipartFile;
 
 import java.time.LocalDate;
 
@@ -12,12 +14,40 @@ public class MemberFixture {
 
     public static Member memberFixture() {
         String encrypted = passwordEncryptor.encrypt(getFixturePlainPassword(), "salt");
-        return new Member(
+        MemberSignUpRequest request = new MemberSignUpRequest(
                 "example@friendy.com",
                 "bokSungKim",
-                encrypted,
-                "salt",
-                LocalDate.parse("2002-08-13")
+                "password123!",
+                LocalDate.parse("2002-08-13"),
+                "https://test.com/test");
+        return new Member(request,encrypted,"salt");
+    }
+
+    public static MockMultipartFile createJsonRequest(String email, String nickname, String password, LocalDate birthDate) {
+        String jsonRequest = String.format("""
+        {
+          "email": "%s",
+          "nickname": "%s",
+          "password": "%s",
+          "birthDate": "%s"
+        }
+        """, email, nickname, password, birthDate);
+
+
+        return new MockMultipartFile(
+                "request",
+                "",
+                "application/json",
+                jsonRequest.getBytes()
+        );
+    }
+
+    public static MockMultipartFile createImageFile(String fileName, String contentType, byte[] content) {
+        return new MockMultipartFile(
+                "image",
+                fileName,
+                contentType,
+                content
         );
     }
 
