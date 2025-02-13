102395c401eb15f6a5c3ae4310f07c786f982009
김재원 <rlawodnjs353@naver.com>
2025-02-13 23:06:50 +0900

    test(#54): 멤버이미지객체 테스트 추가

---
+++ src/test/java/friendy/community/domain/member/model/MemberImageTest.java
@@ -0,0 +1,33 @@
+package friendy.community.domain.member.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberImageTest {
+
+    private MemberImage memberImage;
+
+    @BeforeEach
+    void setUp() {
+        memberImage = MemberImage.of("old-url.com/image.jpg", "old-file-name", "image/jpeg");
+    }
+
+    @Test
+    @DisplayName("updateImage 메서드는 imageUrl, s3Key, fileType을 정상적으로 변경해야 한다.")
+    void updateImage_Should_UpdateFieldsCorrectly() {
+        // given
+        String newImageUrl = "new-url.com/image.jpg";
+        String newS3Key = "new-file-name";
+        String newFileType = "image/png";
+
+        // when
+        memberImage.updateImage(newImageUrl, newS3Key, newFileType);
+
+        // then
+        assertThat(memberImage.getImageUrl()).isEqualTo(newImageUrl);
+        assertThat(memberImage.getS3Key()).isEqualTo(newS3Key);
+        assertThat(memberImage.getFileType()).isEqualTo(newFileType);
+    }
+}
