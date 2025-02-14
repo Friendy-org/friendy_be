3e3da13ca451e14ba5475931a59f34f7c848eb11
jw <rlawodnjs353@naver.com>
2025-02-14 11:29:18 +0900

    style(#54): S3exceptiontest 함수명 수정

--- src/test/java/friendy/community/infra/storage/s3/exception/S3exceptionTest.java
+++ src/test/java/friendy/community/infra/storage/s3/exception/S3exceptionTest.java
@@ -2,19 +2,23 @@ package friendy.community.infra.storage.s3.exception;
 
 import friendy.community.global.exception.ErrorCode;
 import friendy.community.global.exception.FriendyException;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.web.multipart.MultipartFile;
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@Transactional
+@DirtiesContext
 class S3exceptionTest {
 
     @Autowired
@@ -23,43 +27,41 @@ class S3exceptionTest {
     @Mock
     private MultipartFile multipartFile;
 
-    @BeforeEach
-    void setUp() {
-        s3exception = new S3exception();
-    }
-
     @Test
-    void validateFile_파일이비어있을때_예외() {
+    @DisplayName("파일이 비어있을 때 예외가 발생한다.")
+    void thorwsExceptionWhenFileDosentExists() {
         // Given
         when(multipartFile.isEmpty()).thenReturn(true);
 
-        // When & Then
+        // When
         FriendyException exception = assertThrows(FriendyException.class, () -> {
             s3exception.validateFile(multipartFile);
         });
 
-        // 예외 검증
+        // Then
         assertEquals(ErrorCode.INVALID_FILE, exception.getErrorCode());
         assertEquals("파일이 비어 있습니다.", exception.getMessage());
     }
 
     @Test
-    void validateFile_지원되지않는확장자일때_예외() {
+    @DisplayName("파일 크기가 제한을 초과하면 예외를 발생시킨다.")
+    void throwsExceptionWhenUnsupportedFileExtension() {
         // Given
         when(multipartFile.getOriginalFilename()).thenReturn("test.xyz");
 
-        // When & Then
+        // When
         FriendyException exception = assertThrows(FriendyException.class, () -> {
             s3exception.validateFile(multipartFile);
         });
 
-        // 예외 검증
+        // Then
         assertEquals(ErrorCode.INVALID_FILE, exception.getErrorCode());
         assertEquals("지원되지 않는 파일 확장자입니다.", exception.getMessage());
     }
 
     @Test
-    void validateFile_파일크기가초과할때_예외() {
+    @DisplayName("파일 크기가 제한을 초과하면 예외를 발생시킨다.")
+    void throwsExceptionWhenFileSizeExceedsLimit() {
         // Given
         int MAX_FILE_SIZE = 1024 * 1024;
 
@@ -67,30 +69,31 @@ class S3exceptionTest {
                 "file", "test.jpg", "image/jpeg", new byte[MAX_FILE_SIZE+1] // 파일 이름과 MIME 타입 설정
         );
 
-        // When & Then
+        // When
         FriendyException exception = assertThrows(FriendyException.class, () -> {
             s3exception.validateFile(multipartFile);
         });
 
-        // 예외 검증
+        // Then
         assertEquals(ErrorCode.INVALID_FILE, exception.getErrorCode());
         assertEquals("파일 크기가 허용된 범위를 초과했습니다.", exception.getMessage());
     }
 
     @Test
-    void validateFile_지원되지않는MIME타입일때_예외() {
+    @DisplayName("지원되지 않는 MIME 타입일 경우 예외를 발생시킨다.")
+    void throwsExceptionWhenUnsupportedMimeType() {
         // Given
         MockMultipartFile multipartFile = new MockMultipartFile(
                 "file", "test.jpg", "application/zip", "test data".getBytes() // 파일 이름과 MIME 타입 설정
         );
 
-        // When & Then
+        // When
         FriendyException exception = assertThrows(FriendyException.class, () -> {
             // multipartFile을 mock으로 사용할 때 getContentType() 메서드를 호출하도록 설정
             s3exception.validateFile(multipartFile);  // 예외가 발생해야 한다.
         });
 
-        // 예외 검증
+        // Then
         assertEquals(ErrorCode.INVALID_FILE, exception.getErrorCode());
         assertEquals("지원되지 않는 파일 형식입니다.", exception.getMessage());
     }
