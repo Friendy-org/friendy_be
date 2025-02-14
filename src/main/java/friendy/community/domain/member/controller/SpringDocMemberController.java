a8d69a588b347f2c96f9d7ef75e1c193b5fb0df0
jw <rlawodnjs353@naver.com>
2025-02-14 10:01:23 +0900

    docs(#54): 멤버 컨트롤러 문서화 추가

--- src/main/java/friendy/community/domain/member/controller/SpringDocMemberController.java
+++ src/main/java/friendy/community/domain/member/controller/SpringDocMemberController.java
@@ -26,10 +26,16 @@ public interface SpringDocMemberController {
             @ErrorCase(description = "비밀번호 형식 오류", exampleMessage = "숫자, 영문자, 특수문자(~!@#$%^&*?)를 포함해야 합니다."),
             @ErrorCase(description = "비밀번호 글자수 오류", exampleMessage = "비밀번호는 8~16자 사이로 입력해주세요."),
             @ErrorCase(description = "생년월일 입력 없음", exampleMessage = "생년월일이 입력되지 않았습니다."),
+            @ErrorCase(description = "유효한 URL 형식이어야 합니다.", exampleMessage = "유효한 URL 형식이어야 합니다."),
+            @ErrorCase(description = "파일타입을 가져올수 없습니다.", exampleMessage = "파일타입을 가져올수 없습니다.")
+
     })
     @ApiErrorResponse(status = HttpStatus.CONFLICT, instance = "/signup", errorCases = {
             @ErrorCase(description = "이메일 중복", exampleMessage = "이미 가입된 이메일입니다."),
             @ErrorCase(description = "닉네임 중복", exampleMessage = "닉네임이 이미 존재합니다."),
+    })
+    @ApiErrorResponse(status = HttpStatus.INTERNAL_SERVER_ERROR, instance = "/signup", errorCases = {
+        @ErrorCase(description = "S3 객체 복사에 실패했습니다", exampleMessage = "S3 객체 복사에 실패했습니다")
     })
     ResponseEntity<Void> signUp(MemberSignUpRequest request);
 
