4097f076c72617ff0720cf47a83ef9fac4d04ce5
jw <rlawodnjs353@naver.com>
2025-02-14 09:12:35 +0900

    style(#54): s3service 필요없는 주석제거

--- src/main/java/friendy/community/infra/storage/s3/service/S3service.java
+++ src/main/java/friendy/community/infra/storage/s3/service/S3service.java
@@ -33,23 +33,9 @@ public class S3service {
     private final S3exception s3exception;
 
     public String upload(MultipartFile multipartFile, String dirName) {
-
         s3exception.validateFile(multipartFile);
 
         return putS3(multipartFile, generateStoredFileName(multipartFile,dirName));
-    }
-
-    private String putS3(MultipartFile multipartFile, String uuidFileName) {
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(multipartFile.getSize());
-        metadata.setContentType(multipartFile.getContentType());
-
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            s3Client.putObject(bucket, uuidFileName, inputStream, metadata); // UUID 파일명으로 S3에 저장
-        } catch (IOException e) {
-            throw new FriendyException(ErrorCode.FILE_IO_ERROR, "S3 업로드 중 오류 발생");
-        }
-        return s3Client.getUrl(bucket, uuidFileName).toString(); // UUID 파일명으로 URL 반환
     }
 
     public String generateStoredFileName(MultipartFile multipartFile, String dirName) {
@@ -60,29 +46,17 @@ public class S3service {
         }
         String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
         String uuid = UUID.randomUUID().toString();
-        return dirName + "/" + uuid + extension; // 예: 123e4567-e89b-12d3-a456-426614174000.jpg
+        return dirName + "/" + uuid + extension;
     }
 
     public String moveS3Object(String imageUrl, String newDirName) {
         String oldKey = extractFilePath(imageUrl);
-
-        String fileName = oldKey.substring(oldKey.lastIndexOf("/") + 1); // 파일명만 추출
-        String newKey = newDirName + "/" + fileName; // 새로운 경로 생성
+        String fileName = oldKey.substring(oldKey.lastIndexOf("/") + 1);
+        String newKey = newDirName + "/" + fileName;
 
         copyObject(bucket, oldKey, bucket, newKey);
 
         return s3Client.getUrl(bucket, newKey).toString();
-    }
-
-    private void copyObject(String sourceBucket, String sourceKey, String destinationBucket, String destinationKey) {
-        CopyObjectRequest copyObjectRequest = new CopyObjectRequest(
-                sourceBucket, sourceKey,
-                destinationBucket, destinationKey);
-        try {
-            s3Client.copyObject(copyObjectRequest);
-        } catch (FriendyException e) {
-            throw new FriendyException(ErrorCode.INTERNAL_SERVER_ERROR,"S3 객체 복사에 실패했습니다");
-        }
     }
 
     public String extractFilePath(String imageUrl) {
@@ -95,13 +69,37 @@ public class S3service {
         }
     }
 
-
     public String getContentTypeFromS3(String key) {
         try {
             S3Object object = s3Client.getObject(new GetObjectRequest(bucket, key));
             return object.getObjectMetadata().getContentType();
         } catch (Exception e) {
             throw new FriendyException(ErrorCode.INVALID_FILE, "파일타입을 가져올수 없습니다.");
+        }
+    }
+
+    private String putS3(MultipartFile multipartFile, String uuidFileName) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            s3Client.putObject(bucket, uuidFileName, inputStream, metadata);
+        } catch (IOException e) {
+            throw new FriendyException(ErrorCode.FILE_IO_ERROR, "S3 업로드 중 오류 발생");
+        }
+        return s3Client.getUrl(bucket, uuidFileName).toString();
+    }
+
+    private void copyObject(String sourceBucket, String sourceKey, String destinationBucket, String destinationKey) {
+        CopyObjectRequest copyObjectRequest = new CopyObjectRequest(
+            sourceBucket, sourceKey,
+            destinationBucket, destinationKey);
+
+        try {
+            s3Client.copyObject(copyObjectRequest);
+        } catch (FriendyException e) {
+            throw new FriendyException(ErrorCode.INTERNAL_SERVER_ERROR,"S3 객체 복사에 실패했습니다");
         }
     }
 }
