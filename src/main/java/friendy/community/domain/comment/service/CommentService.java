288a1f1cc5f633dd585a3a640b7a4c79127d4708
Wooki <hyunook02@naver.com>
2025-02-15 15:44:09 +0900

    feat(#61): postId로 대상 게시글 찾아서 저장하도록 변경

--- src/main/java/friendy/community/domain/comment/service/CommentService.java
+++ src/main/java/friendy/community/domain/comment/service/CommentService.java
@@ -7,6 +7,10 @@ import friendy.community.domain.comment.dto.CommentCreateRequest;
 import friendy.community.domain.comment.model.Comment;
 import friendy.community.domain.comment.repository.CommentRepository;
 import friendy.community.domain.member.model.Member;
+import friendy.community.domain.post.model.Post;
+import friendy.community.domain.post.repository.PostRepository;
+import friendy.community.global.exception.ErrorCode;
+import friendy.community.global.exception.FriendyException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +26,12 @@ public class CommentService {
     private final JwtTokenProvider jwtTokenProvider;
     private final AuthService authService;
 
+    private final PostRepository postRepository;
+
     public long saveComment(final CommentCreateRequest commentCreateRequest, final HttpServletRequest httpServletRequest) {
         final Member member = getMemberFromRequest(httpServletRequest);
-        final Comment comment = Comment.of(commentCreateRequest, member);
+        final Post post = getPostByPostId(commentCreateRequest.postId());
+        final Comment comment = Comment.of(commentCreateRequest, member, post);
         commentRepository.save(comment);
 
         return comment.getId();
@@ -34,6 +41,11 @@ public class CommentService {
         final String accessToken = jwtTokenExtractor.extractAccessToken(httpServletRequest);
         final String email = jwtTokenProvider.extractEmailFromAccessToken(accessToken);
         return authService.getMemberByEmail(email);
+    }
+
+    private Post getPostByPostId(final Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "댓글 작성 대상 게시글이 존재하지 않습니다."));
     }
 
 }
