25008afaa4abcb3921aa566ecb8c82de54d03b9d
Wooki <hyunook02@naver.com>
2025-02-23 16:08:05 +0900

    feat(#61): 댓글 id 통해 댓글 객체 찾는 기능 추가

--- src/main/java/friendy/community/domain/comment/repository/CommentRepository.java
+++ src/main/java/friendy/community/domain/comment/repository/CommentRepository.java
@@ -3,5 +3,10 @@ package friendy.community.domain.comment.repository;
 import friendy.community.domain.comment.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Optional<Comment> findById(Long id);
+
 }
