5176beea30d867dee0a9fd50b231fe374f719be7
Wooki <hyunook02@naver.com>
2025-02-23 19:43:40 +0900

    docs(#61): 답글 api 스웨거 문서 작성

--- src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
+++ src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
@@ -40,6 +40,16 @@ public interface SpringDocCommentController {
             @ErrorCase(description = "답글 내용 없음", exampleMessage = "답글 내용이 입력되지 않았습니다."),
             @ErrorCase(description = "답글 내용 길이 초과", exampleMessage = "답글은 1100자 이내로 작성해주세요."),
             @ErrorCase(description = "답글 작성 대상 게시글 누락", exampleMessage = "답글이 달릴 게시글이 명시되지 않았습니다."),
+            @ErrorCase(description = "답글 작성 대상 댓글 누락", exampleMessage = "답글이 달릴 댓글이 명시되지 않았습니다."),
+    })
+    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments", errorCases = {
+            @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
+            @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
+            @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다.")
+    })
+    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments", errorCases = {
+            @ErrorCase(description = "잘못된 게시글 id", exampleMessage = "댓글 작성 대상 게시글이 존재하지 않습니다."),
+            @ErrorCase(description = "잘못된 댓글 id", exampleMessage = "존재하지 않는 댓글입니다.")
     })
     ResponseEntity<Void> createReply(
             HttpServletRequest httpServletRequest,
