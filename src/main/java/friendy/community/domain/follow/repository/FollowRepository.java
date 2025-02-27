5c5f3db555cdf71cc68ec21ebc10f13429dd2b67
jw <rlawodnjs353@naver.com>
2025-02-27 10:26:43 +0900

    feat(#72): existsByFollowerAndFollowing,findByFollowerAndFollowing 추가

--- src/main/java/friendy/community/domain/follow/repository/FollowRepository.java
+++ src/main/java/friendy/community/domain/follow/repository/FollowRepository.java
@@ -1,9 +1,15 @@
 package friendy.community.domain.follow.repository;
 
 import friendy.community.domain.follow.model.Follow;
+import friendy.community.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    boolean existsByFollowerAndFollowing(Member follower, Member following);
+
+    Optional<Follow> findByFollowerAndFollowing(Member follower, Member following);
 }
