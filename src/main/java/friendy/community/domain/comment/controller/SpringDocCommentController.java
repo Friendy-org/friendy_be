7d80f021da3cefbd2350d1e8cd32d167892ba61e
Wooki <hyunook02@naver.com>
2025-02-27 18:37:14 +0900

    feat(#73): 댓글, 답글 수정 api 작성

--- src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
+++ src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
@@ -1,6 +1,7 @@
 package friendy.community.domain.comment.controller;
 
 import friendy.community.domain.comment.dto.CommentCreateRequest;
+import friendy.community.domain.comment.dto.CommentUpdateRequest;
 import friendy.community.domain.comment.dto.ReplyCreateRequest;
 import friendy.community.global.swagger.error.ApiErrorResponse;
 import friendy.community.global.swagger.error.ErrorCase;
@@ -10,12 +11,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "댓글 API", description = "댓글 API")
 public interface SpringDocCommentController {
     @Operation(summary = "댓글 작성")
-    @ApiResponse(responseCode = "201", description = "댓글 작성 성공")
+    @ApiResponse(responseCode = "200", description = "댓글 작성 성공")
     @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/comments", errorCases = {
             @ErrorCase(description = "댓글 내용 없음", exampleMessage = "댓글 내용이 입력되지 않았습니다."),
             @ErrorCase(description = "댓글 내용 길이 초과", exampleMessage = "댓글은 1100자 이내로 작성해주세요."),
@@ -34,22 +36,43 @@ public interface SpringDocCommentController {
             @RequestBody CommentCreateRequest commentRequest
     );
 
+    @Operation(summary = "댓글(답글) 수정")
+    @ApiResponse(responseCode = "200", description = "댓글(답글) 수정 성공")
+    @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "댓글 내용 없음", exampleMessage = "댓글(답글) 내용이 입력되지 않았습니다."),
+            @ErrorCase(description = "댓글 내용 길이 초과", exampleMessage = "댓글(답글)은 1100자 이내로 작성해주세요.")
+    })
+    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
+            @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
+            @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다."),
+            @ErrorCase(description = "다른 사용자의 댓글(답글) 수정 시도", exampleMessage = "작성자만 댓글(답글)을 수정할 수 있습니다.")
+    })
+    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "잘못된 댓글(답글) id", exampleMessage = "존재하지 않는 댓글(답글)입니다.")
+    })
+    ResponseEntity<Void> updateComment(
+            HttpServletRequest httpServletRequest,
+            @PathVariable Long commentId,
+            @RequestBody CommentUpdateRequest commentUpdateRequest
+    );
+
     @Operation(summary = "답글 작성")
-    @ApiResponse(responseCode = "201", description = "답글 작성 성공")
+    @ApiResponse(responseCode = "200", description = "답글 작성 성공")
     @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/comments/reply", errorCases = {
             @ErrorCase(description = "답글 내용 없음", exampleMessage = "답글 내용이 입력되지 않았습니다."),
             @ErrorCase(description = "답글 내용 길이 초과", exampleMessage = "답글은 1100자 이내로 작성해주세요."),
             @ErrorCase(description = "답글 작성 대상 게시글 누락", exampleMessage = "답글이 달릴 게시글이 명시되지 않았습니다."),
             @ErrorCase(description = "답글 작성 대상 댓글 누락", exampleMessage = "답글이 달릴 댓글이 명시되지 않았습니다."),
     })
-    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments", errorCases = {
+    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/reply", errorCases = {
             @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
             @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
             @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다.")
     })
-    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments", errorCases = {
+    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/reply", errorCases = {
             @ErrorCase(description = "잘못된 게시글 id", exampleMessage = "댓글 작성 대상 게시글이 존재하지 않습니다."),
-            @ErrorCase(description = "잘못된 댓글 id", exampleMessage = "존재하지 않는 댓글입니다.")
+            @ErrorCase(description = "잘못된 댓글 id", exampleMessage = "존재하지 않는 댓글(답글)입니다.")
     })
     ResponseEntity<Void> createReply(
             HttpServletRequest httpServletRequest,
--- src/main/java/friendy/community/domain/comment/dto/CommentUpdateRequest.java
+++ src/main/java/friendy/community/domain/comment/dto/CommentUpdateRequest.java
@@ -3,13 +3,14 @@ package friendy.community.domain.comment.dto;
 import friendy.community.domain.comment.CommentType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-@Schema(description = "댓글 수정")
+@Schema(description = "댓글(답글) 수정")
 public record CommentUpdateRequest(
-        @Schema(description = "댓글 내용", example = "수정된 댓글 내용")
-        @NotBlank(message = "댓글 내용이 입력되지 않았습니다.")
-        @Size(max = 1100, message = "댓글은 1100자 이내로 작성해주세요.")
+        @Schema(description = "댓글(답글) 내용", example = "수정된 댓글(답글) 내용")
+        @NotBlank(message = "댓글(답글) 내용이 입력되지 않았습니다.")
+        @Size(max = 1100, message = "댓글(답글)은 1100자 이내로 작성해주세요.")
         String content
 ) {
 }
