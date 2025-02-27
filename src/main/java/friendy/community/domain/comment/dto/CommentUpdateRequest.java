261103a823122e1069c2a7688d7d10fcd3a82b37
Wooki <hyunook02@naver.com>
2025-02-27 17:56:49 +0900

    feat(#73): 댓글 및 답글 수정 dto 작성

--- src/main/java/friendy/community/domain/comment/dto/CommentUpdateRequest.java
+++ src/main/java/friendy/community/domain/comment/dto/CommentUpdateRequest.java
@@ -7,13 +7,9 @@ import jakarta.validation.constraints.Size;
 
 @Schema(description = "댓글 수정")
 public record CommentUpdateRequest(
-        @Schema(description = "댓글 내용", example = "프렌디댓글내용")
+        @Schema(description = "댓글 내용", example = "수정된 댓글 내용")
         @NotBlank(message = "댓글 내용이 입력되지 않았습니다.")
         @Size(max = 1100, message = "댓글은 1100자 이내로 작성해주세요.")
-        String content,
-
-        @Schema(description = "댓글 종류", example = "COMMENT or REPLY")
-        @NotBlank(message = "댓글 종류가 입력되지 않았습니다.")
-        CommentType type
+        String content
 ) {
 }
