cb42fb84118ab1de5c2903003634f74d8ca7ccfa
Wooki <hyunook02@naver.com>
2025-03-01 17:24:41 +0900

    feat(#73): 댓글 수정 api 테스트 작성

--- src/test/java/friendy/community/domain/comment/controller/CommentControllerTest.java
+++ src/test/java/friendy/community/domain/comment/controller/CommentControllerTest.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import friendy.community.domain.comment.CommentType;
 import friendy.community.domain.comment.dto.CommentCreateRequest;
+import friendy.community.domain.comment.dto.CommentUpdateRequest;
 import friendy.community.domain.comment.dto.ReplyCreateRequest;
+import friendy.community.domain.comment.fixture.CommentFixture;
+import friendy.community.domain.comment.model.Comment;
 import friendy.community.domain.comment.service.CommentService;
+import friendy.community.domain.post.dto.request.PostUpdateRequest;
 import friendy.community.global.exception.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -17,7 +21,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -119,7 +123,7 @@ public class CommentControllerTest {
 
     @Test
     @DisplayName("답글 내용이 1100자 초과 시 400 Bad Request 응답")
-    void createReplyWithContentExeedingMaxLengthReturns400BadRequest() throws Exception {
+    void createReplyWithContentExceedingMaxLengthReturns400BadRequest() throws Exception {
         // Given
         ReplyCreateRequest request = new ReplyCreateRequest("a".repeat(1200), 1L, 1L);
 
@@ -155,6 +159,57 @@ public class CommentControllerTest {
         mockMvc.perform(post("/comments/reply")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("댓글(답글) 수정 성공 시 200 Ok 응답")
+    void updateCommentSuccessfullyReturns200Ok() throws Exception {
+        // Given
+        Long commentId = 1L;
+        CommentUpdateRequest updateRequest = new CommentUpdateRequest("edited valid content");
+
+        doNothing().when(commentService).updateComment(any(CommentUpdateRequest.class), eq(commentId), any(HttpServletRequest.class));
+
+        // When & Then
+        mockMvc.perform(post("/comments/{commentId}", commentId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("빈 내용으로 댓글(답글) 수정 요청 시 400 Bad Request 응답")
+    void updateCommentWithoutContentReturns400BadRequest() throws Exception {
+        // Given
+        Long commentId = 1L;
+        CommentUpdateRequest updateRequest = new CommentUpdateRequest("");
+
+        doNothing().when(commentService).updateComment(any(CommentUpdateRequest.class), eq(commentId), any(HttpServletRequest.class));
+
+        // When & Then
+        mockMvc.perform(post("/comments/{commentId}", commentId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("댓글(답글)수정 요청의 내용이 제한을 초과하는 경우 400 Bad Request 응답")
+    void updateCommentWithExceedingContentsLengthLimitReturns400BadRequest() throws Exception {
+        // Given
+        Long commentId = 1L;
+        CommentUpdateRequest updateRequest = new CommentUpdateRequest("a".repeat(1200));
+
+        doNothing().when(commentService).updateComment(any(CommentUpdateRequest.class), eq(commentId), any(HttpServletRequest.class));
+
+        // When & Then
+        mockMvc.perform(post("/comments/{commentId}", commentId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
     }
