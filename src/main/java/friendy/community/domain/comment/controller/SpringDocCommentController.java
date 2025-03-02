bbafd148396ea4b69fcfc0e88deedc4dabd6f436
Wooki <hyunook02@naver.com>
2025-03-02 17:37:23 +0900

    docs(#75): 스프링 문서 작성

--- src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
+++ src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
@@ -36,20 +36,20 @@ public interface SpringDocCommentController {
             @RequestBody CommentCreateRequest commentRequest
     );
 
-    @Operation(summary = "댓글(답글) 수정")
-    @ApiResponse(responseCode = "200", description = "댓글(답글) 수정 성공")
+    @Operation(summary = "댓글 수정")
+    @ApiResponse(responseCode = "200", description = "댓글 수정 성공")
     @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/comments/{commentId}", errorCases = {
-            @ErrorCase(description = "댓글 내용 없음", exampleMessage = "댓글(답글) 내용이 입력되지 않았습니다."),
-            @ErrorCase(description = "댓글 내용 길이 초과", exampleMessage = "댓글(답글)은 1100자 이내로 작성해주세요.")
+            @ErrorCase(description = "댓글 내용 없음", exampleMessage = "댓글 내용이 입력되지 않았습니다."),
+            @ErrorCase(description = "댓글 내용 길이 초과", exampleMessage = "댓글은 1100자 이내로 작성해주세요.")
     })
     @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/{commentId}", errorCases = {
             @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
             @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
             @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다."),
-            @ErrorCase(description = "다른 사용자의 댓글(답글) 수정 시도", exampleMessage = "작성자만 댓글(답글)을 수정할 수 있습니다.")
+            @ErrorCase(description = "다른 사용자의 댓글 수정 시도", exampleMessage = "작성자만 댓글을 수정할 수 있습니다.")
     })
     @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/{commentId}", errorCases = {
-            @ErrorCase(description = "잘못된 댓글(답글) id", exampleMessage = "존재하지 않는 댓글(답글)입니다.")
+            @ErrorCase(description = "잘못된 댓글 id", exampleMessage = "존재하지 않는 댓글입니다.")
     })
     ResponseEntity<Void> updateComment(
             HttpServletRequest httpServletRequest,
@@ -72,10 +72,31 @@ public interface SpringDocCommentController {
     })
     @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/reply", errorCases = {
             @ErrorCase(description = "잘못된 게시글 id", exampleMessage = "댓글 작성 대상 게시글이 존재하지 않습니다."),
-            @ErrorCase(description = "잘못된 댓글 id", exampleMessage = "존재하지 않는 댓글(답글)입니다.")
+            @ErrorCase(description = "잘못된 댓글 id", exampleMessage = "존재하지 않는 댓글입니다.")
     })
     ResponseEntity<Void> createReply(
             HttpServletRequest httpServletRequest,
             @RequestBody ReplyCreateRequest replyRequest
+    );
+
+    @Operation(summary = "답글 수정")
+    @ApiResponse(responseCode = "200", description = "답글 수정 성공")
+    @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "답글 내용 없음", exampleMessage = "답글 내용이 입력되지 않았습니다."),
+            @ErrorCase(description = "답글 내용 길이 초과", exampleMessage = "답글은 1100자 이내로 작성해주세요.")
+    })
+    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
+            @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
+            @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다."),
+            @ErrorCase(description = "다른 사용자의 답글 수정 시도", exampleMessage = "작성자만 답글을 수정할 수 있습니다.")
+    })
+    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "잘못된 답글 id", exampleMessage = "존재하지 않는 답글입니다.")
+    })
+    ResponseEntity<Void> updateReply(
+            HttpServletRequest httpServletRequest,
+            @PathVariable Long commentId,
+            @RequestBody CommentUpdateRequest commentUpdateRequest
     );
 }
