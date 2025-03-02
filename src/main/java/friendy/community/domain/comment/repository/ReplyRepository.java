afe527c3eac81be5f1e17112c8049cdf07941007
Wooki <hyunook02@naver.com>
2025-03-02 16:54:02 +0900

    feat(#75): Reply 모델, 리포지토리 작성

---
+++ src/main/java/friendy/community/domain/comment/repository/ReplyRepository.java
@@ -0,0 +1,10 @@
+package friendy.community.domain.comment.repository;
+
+import friendy.community.domain.comment.model.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    Optional<Reply> findbyId(Long id);
+}
