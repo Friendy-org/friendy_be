73bf07916a1d3cb47bdd464b70337e3ed5537289
Wooki <hyunook02@naver.com>
2025-03-09 17:00:43 +0900

    feat(#62): 댓글, 답글 삭제 api 구현

--- src/main/java/friendy/community/domain/comment/service/CommentService.java
+++ src/main/java/friendy/community/domain/comment/service/CommentService.java
@@ -17,6 +17,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -61,6 +63,28 @@ public class CommentService {
 
         reply.updateContent(commentUpdateRequest.content());
         replyRepository.save(reply);
+    }
+
+    public void deleteComment(final Long commentId, final HttpServletRequest httpServletRequest) {
+        final Member member = getMemberFromRequest(httpServletRequest);
+        final Comment comment = getCommentByCommentId(commentId);
+        validateAuthor(comment, member);
+
+        List<Reply> replies = replyRepository.findAllByComment(comment);
+        replyRepository.deleteAll(replies);
+        
+        commentRepository.delete(comment);
+    }
+
+    public void deleteReply(final Long replyId, final HttpServletRequest httpServletRequest) {
+        final Member member = getMemberFromRequest(httpServletRequest);
+        final Reply reply = getReplyByReplyId(replyId);
+        validateAuthor(reply, member);
+
+        Comment comment = reply.getComment();
+        comment.updateReplyCount(comment.getReplyCount() - 1);
+
+        replyRepository.delete(reply);
     }
 
     private void validateAuthor(final Comment comment, final Member member) {
