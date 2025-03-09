366af9eefe4f8e92e1194b5a1bb8129d2acf209a
Wooki <hyunook02@naver.com>
2025-03-09 17:23:53 +0900

    docs(#62): spring api doc 작성

--- src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
+++ src/main/java/friendy/community/domain/comment/controller/SpringDocCommentController.java
@@ -83,22 +83,54 @@ public interface SpringDocCommentController {
 
     @Operation(summary = "답글 수정")
     @ApiResponse(responseCode = "200", description = "답글 수정 성공")
-    @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/comments/{commentId}", errorCases = {
+    @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/comments/reply/{commentId}", errorCases = {
             @ErrorCase(description = "답글 내용 없음", exampleMessage = "답글 내용이 입력되지 않았습니다."),
             @ErrorCase(description = "답글 내용 길이 초과", exampleMessage = "답글은 1100자 이내로 작성해주세요.")
     })
-    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/{commentId}", errorCases = {
+    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/reply/{commentId}", errorCases = {
             @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
             @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
             @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다."),
             @ErrorCase(description = "다른 사용자의 답글 수정 시도", exampleMessage = "작성자만 답글을 수정할 수 있습니다.")
     })
-    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/{commentId}", errorCases = {
+    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/reply/{commentId}", errorCases = {
             @ErrorCase(description = "잘못된 답글 id", exampleMessage = "존재하지 않는 답글입니다.")
     })
     ResponseEntity<Void> updateReply(
             @AuthenticationPrincipal FriendyUserDetails userDetails,
             @PathVariable Long commentId,
             @RequestBody CommentUpdateRequest commentUpdateRequest
+    );
+
+    @Operation(summary = "댓글 삭제")
+    @ApiResponse(responseCode = "200", description = "댓글 삭제 성공")
+    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
+            @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
+            @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다."),
+            @ErrorCase(description = "다른 사용자의 댓글 삭제 시도", exampleMessage = "작성자만 댓글을 수정할 수 있습니다.")
+    })
+    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/{commentId}", errorCases = {
+            @ErrorCase(description = "잘못된 댓글 id", exampleMessage = "존재하지 않는 댓글입니다.")
+    })
+    ResponseEntity<Void> deleteComment(
+            HttpServletRequest httpServletRequest,
+            @PathVariable Long commentId
+    );
+
+    @Operation(summary = "답글 삭제")
+    @ApiResponse(responseCode = "200", description = "답글 삭제 성공")
+    @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/comments/reply/{replyId}", errorCases = {
+            @ErrorCase(description = "액세스 토큰 추출 실패", exampleMessage = "인증 실패(액세스 토큰 추출 실패) - 토큰 : {token}"),
+            @ErrorCase(description = "JWT 액세스 토큰 Payload 이메일 누락", exampleMessage = "인증 실패(JWT 액세스 토큰 Payload 이메일 누락) - 토큰 : {token}"),
+            @ErrorCase(description = "조작된 액세스 토큰", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다."),
+            @ErrorCase(description = "다른 사용자의 답글 삭제 시도", exampleMessage = "작성자만 답글을 수정할 수 있습니다.")
+    })
+    @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/comments/reply/{replyId}", errorCases = {
+            @ErrorCase(description = "잘못된 답글 id", exampleMessage = "존재하지 않는 답글입니다.")
+    })
+    ResponseEntity<Void> deleteReply(
+            HttpServletRequest httpServletRequest,
+            @PathVariable Long replyId
     );
 }
