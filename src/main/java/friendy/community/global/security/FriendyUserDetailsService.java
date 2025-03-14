cbe865ee56cda12b221d8de1d850956ab1401031
jw <rlawodnjs353@naver.com>
2025-03-14 10:21:39 +0900

    feat(#78) UserDetailsService loadUserByUsername 추가

---
+++ src/main/java/friendy/community/global/security/FriendyUserDetailsService.java
@@ -0,0 +1,28 @@
+package friendy.community.global.security;
+
+import friendy.community.domain.member.model.Member;
+import friendy.community.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class FriendyUserDetailsService implements UserDetailsService {
+
+    private final MemberService memberService;
+
+    @Override
+    public FriendyUserDetails loadUserByUsername(String email) {
+        Member member = memberService.findMemberByEmail(email);
+
+        return new FriendyUserDetails(
+            member.getId(),  // memberId
+            member.getEmail(),  // 이메일
+            member.getPassword(),  // 비밀번호
+            Collections.emptyList()
+        );
+    }
+}
