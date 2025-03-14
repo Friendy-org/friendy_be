1156efe702491f55a5654608a476ea19eb979bfd
jw <rlawodnjs353@naver.com>
2025-03-14 10:33:21 +0900

    refactor(#78): 토큰인터셉트로 인한 Follow 수정

--- src/main/java/friendy/community/domain/follow/service/FollowService.java
+++ src/main/java/friendy/community/domain/follow/service/FollowService.java
@@ -1,17 +1,14 @@
 package friendy.community.domain.follow.service;
 
-import friendy.community.domain.auth.jwt.JwtTokenExtractor;
-import friendy.community.domain.auth.jwt.JwtTokenProvider;
-import friendy.community.domain.auth.service.AuthService;
 import friendy.community.domain.follow.dto.response.FollowListResponse;
 import friendy.community.domain.follow.model.Follow;
 import friendy.community.domain.follow.repository.FollowQueryDSLRepository;
 import friendy.community.domain.follow.repository.FollowRepository;
 import friendy.community.domain.member.model.Member;
 import friendy.community.domain.member.repository.MemberRepository;
+import friendy.community.domain.member.service.MemberService;
 import friendy.community.global.exception.ErrorCode;
 import friendy.community.global.exception.FriendyException;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,19 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class FollowService {
 
-    private final JwtTokenExtractor jwtTokenExtractor;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final AuthService authService;
     private final MemberRepository memberRepository;
     private final FollowRepository followRepository;
     private final FollowQueryDSLRepository followQueryDSLRepository;
+    private final MemberService memberService;
 
-    public void follow(final HttpServletRequest httpServletRequest, final Long memberId) {
-        final String accessToken = jwtTokenExtractor.extractAccessToken(httpServletRequest);
-        final String email = jwtTokenProvider.extractEmailFromAccessToken(accessToken);
-        Member follower = authService.getMemberByEmail(email);
+    public void follow(final Long memberId, final Long targetId) {
 
-        Member following = getValidTargetMember(follower, memberId);
+        Member follower = memberService.findMemberById(memberId);
+
+        Member following = getValidTargetMember(follower, targetId);
 
         if (followRepository.existsByFollowerAndFollowing(follower, following)) {
             throw new FriendyException(ErrorCode.ALREADY_EXISTS, "이미 팔로우한 회원입니다.");
@@ -43,11 +37,10 @@ public class FollowService {
         followRepository.save(follow);
     }
 
-    public void unfollow(final HttpServletRequest httpServletRequest, final Long memberId) {
-        final String accessToken = jwtTokenExtractor.extractAccessToken(httpServletRequest);
-        final String email = jwtTokenProvider.extractEmailFromAccessToken(accessToken);
-        Member follower = authService.getMemberByEmail(email);
-        Member following = getValidTargetMember(follower, memberId);
+    public void unfollow(final Long memberId, final Long targetId) {
+
+        Member follower = memberService.findMemberById(memberId);
+        Member following = getValidTargetMember(follower, targetId);
 
         Follow follow = followRepository.findByFollowerAndFollowing(follower, following)
             .orElseThrow(() -> new FriendyException(ErrorCode.ALREADY_EXISTS, "팔로우하지 않은 회원입니다."));
@@ -55,20 +48,20 @@ public class FollowService {
         followRepository.delete(follow);
     }
 
-    public FollowListResponse getFollowingMembers(final Long memberId, final Long startIndex, final int pageSize) {
-        if (!memberRepository.existsById(memberId)) {
+    public FollowListResponse getFollowingMembers(final Long targetId, final Long startIndex, final int pageSize) {
+        if (!memberRepository.existsById(targetId)) {
             throw new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "해당 ID의 회원을 찾을 수 없습니다.");
         }
 
-        return followQueryDSLRepository.findFollowingMembers(memberId, startIndex, pageSize);
+        return followQueryDSLRepository.findFollowingMembers(targetId, startIndex, pageSize);
     }
 
-    public FollowListResponse getFollowerMembers(final Long memberId, final Long startIndex, final int pageSize) {
-        if (!memberRepository.existsById(memberId)) {
+    public FollowListResponse getFollowerMembers(final Long targetId, final Long startIndex, final int pageSize) {
+        if (!memberRepository.existsById(targetId)) {
             throw new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "해당 ID의 회원을 찾을 수 없습니다.");
         }
 
-        return followQueryDSLRepository.findFollowerMembers(memberId, startIndex, pageSize);
+        return followQueryDSLRepository.findFollowerMembers(targetId, startIndex, pageSize);
     }
 
     @Transactional(readOnly = true)
