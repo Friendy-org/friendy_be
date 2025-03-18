6b9cb957f97f5dbcb1c56179b4b098d8665597c5
jw <rlawodnjs353@naver.com>
2025-03-18 11:02:31 +0900

    refactor(#84): 커서 기반 페이지네이션으로 수정

--- src/main/java/friendy/community/domain/follow/repository/FollowQueryDSLRepository.java
+++ src/main/java/friendy/community/domain/follow/repository/FollowQueryDSLRepository.java
@@ -1,17 +1,20 @@
 package friendy.community.domain.follow.repository;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import friendy.community.domain.follow.dto.response.FollowListResponse;
 import friendy.community.domain.follow.dto.response.FollowMemberResponse;
+import friendy.community.domain.follow.model.Follow;
+import friendy.community.domain.member.model.QMember;
+import friendy.community.domain.member.model.QMemberImage;
+import friendy.community.global.exception.ErrorCode;
+import friendy.community.global.exception.FriendyException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static friendy.community.domain.follow.model.QFollow.follow;
-import static friendy.community.domain.member.model.QMember.member;
-import static friendy.community.domain.member.model.QMemberImage.memberImage;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,53 +22,66 @@ public class FollowQueryDSLRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public FollowListResponse findFollowingMembers(Long memberId, Long startIndex, int pageSize) {
-        List<FollowMemberResponse> members = queryFactory
-            .select(Projections.constructor(
-                FollowMemberResponse.class,
-                member.id,
-                member.nickname,
-                memberImage.imageUrl
-            ))
-            .from(follow)
-            .join(follow.following, member)
-            .leftJoin(member.memberImage, memberImage)
-            .where(follow.follower.id.eq(memberId))
-            .orderBy(follow.createdDate.desc())
-            .offset(startIndex)
-            .limit(pageSize + 1)
+    public FollowListResponse findFollowingMembers(Long memberId, Long lastFollowingId, int size) {
+        List<Follow> follows = queryFactory
+            .selectFrom(follow)
+            .join(follow.following, QMember.member).fetchJoin()
+            .leftJoin(QMember.member.memberImage, QMemberImage.memberImage).fetchJoin()
+            .where(
+                follow.follower.id.eq(memberId),
+                lastFollowingId != null ? follow.id.lt(lastFollowingId) : null
+            )
+            .orderBy(follow.id.desc())
+            .limit(size + 1)
             .fetch();
 
-        boolean hasNext = members.size() > pageSize;
-        if (hasNext) {
-            members.remove(pageSize);
+        if (follows.isEmpty()) {
+            throw new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "팔로잉멤버가 없습니다.");
         }
 
-        return new FollowListResponse(members, hasNext);
+        boolean hasNext = follows.size() > size;
+        if (hasNext) {
+            follows.removeLast();
+        }
+
+        Long newLastFollowId = follows.getLast().getId();
+
+        List<FollowMemberResponse> followMemberResponses = follows.stream()
+            .map(follow -> FollowMemberResponse.from(follow.getFollowing()))
+            .collect(Collectors.toList());
+
+        return new FollowListResponse(followMemberResponses, hasNext, newLastFollowId);
     }
 
-    public FollowListResponse findFollowerMembers(Long memberId, Long startIndex, int pageSize) {
-        List<FollowMemberResponse> members = queryFactory
-            .select(Projections.constructor(
-                FollowMemberResponse.class,
-                member.id,
-                member.nickname,
-                memberImage.imageUrl
-            ))
-            .from(follow)
-            .join(follow.follower, member)
-            .leftJoin(member.memberImage, memberImage)
-            .where(follow.following.id.eq(memberId))
-            .orderBy(follow.createdDate.desc())
-            .offset(startIndex)
-            .limit(pageSize + 1)
+    public FollowListResponse findFollowerMembers(Long memberId, Long lastFollowerId, int size) {
+        List<Follow> follows = queryFactory
+            .selectFrom(follow)
+            .join(follow.follower, QMember.member).fetchJoin()
+            .leftJoin(QMember.member.memberImage, QMemberImage.memberImage).fetchJoin()
+            .where(
+                follow.following.id.eq(memberId),
+                lastFollowerId != null ? follow.id.lt(lastFollowerId) : null
+            )
+            .orderBy(follow.id.desc())
+            .limit(size + 1)
             .fetch();
 
-        boolean hasNext = members.size() > pageSize;
-        if (hasNext) {
-            members.remove(pageSize);
+        if (follows.isEmpty()) {
+            throw new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "팔로워가 없습니다.");
         }
 
-        return new FollowListResponse(members, hasNext);
+
+        boolean hasNext = follows.size() > size;
+        if (hasNext) {
+            follows.remove(follows.size() - 1);
+        }
+
+        Long newLastFollowerId = follows.getLast().getId();
+
+        List<FollowMemberResponse> followMemberResponses = follows.stream()
+            .map(follow -> FollowMemberResponse.from(follow.getFollower()))
+            .collect(Collectors.toList());
+
+        return new FollowListResponse(followMemberResponses, hasNext, newLastFollowerId);
     }
 }
