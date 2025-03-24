97be61971f6d30a7ec63d8e8e9955e5a48f8f082
jw <rlawodnjs353@naver.com>
2025-03-24 17:29:03 +0900

    refactor(#86): auth 성공,예외코드 변경

--- src/main/java/friendy/community/domain/auth/controller/AuthController.java
+++ src/main/java/friendy/community/domain/auth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package friendy.community.domain.auth.controller;
 
+import friendy.community.domain.auth.controller.code.AuthSuccessCode;
 import friendy.community.domain.auth.dto.request.LoginRequest;
 import friendy.community.domain.auth.dto.response.TokenResponse;
 import friendy.community.domain.auth.jwt.JwtTokenExtractor;
 import friendy.community.domain.auth.service.AuthService;
+import friendy.community.global.response.FriendyResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,54 +18,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-public class AuthController implements SpringDocAuthController{
+public class AuthController implements SpringDocAuthController {
 
     private final AuthService authService;
     private final JwtTokenExtractor jwtTokenExtractor;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(
-            @Valid @RequestBody LoginRequest loginRequest
+    public ResponseEntity<FriendyResponse<TokenResponse>> login(
+        @Valid @RequestBody LoginRequest loginRequest
     ) {
-        final TokenResponse response = authService.login(loginRequest);
+        final TokenResponse tokenresponse = authService.login(loginRequest);
 
-        return ResponseEntity.ok()
-                .header("Authorization", "Bearer " + response.accessToken())
-                .header("Authorization-Refresh", "Bearer " + response.refreshToken())
-                .build();
+        FriendyResponse<TokenResponse> response = FriendyResponse.of(AuthSuccessCode.LOGIN_SUCCESS, tokenresponse);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(
+    public ResponseEntity<FriendyResponse<Void>> logout(
         HttpServletRequest httpServletRequest
     ) {
         final String accessToken = jwtTokenExtractor.extractAccessToken(httpServletRequest);
         authService.logout(accessToken);
 
-        return ResponseEntity.ok().build();
+        FriendyResponse<Void> response = FriendyResponse.of(AuthSuccessCode.LOGOUT_SUCCESS);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/token/reissue")
-    public ResponseEntity<Void> reissueToken(
-            HttpServletRequest httpServletRequest
+    public ResponseEntity<FriendyResponse<TokenResponse>> reissueToken(
+        HttpServletRequest httpServletRequest
     ) {
         final String refreshToken = jwtTokenExtractor.extractRefreshToken(httpServletRequest);
-        final TokenResponse response = authService.reissueToken(refreshToken);
+        final TokenResponse tokenresponse = authService.reissueToken(refreshToken);
 
-        return ResponseEntity.ok()
-                .header("Authorization", "Bearer " + response.accessToken())
-                .header("Authorization-Refresh", "Bearer " + response.refreshToken())
-                .build();
+        FriendyResponse<TokenResponse> response = FriendyResponse.of(AuthSuccessCode.TOKEN_REISSUE_SUCCESS, tokenresponse);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/withdrawal")
-    public ResponseEntity<Void> withdrawal(
-            HttpServletRequest httpServletRequest
+    public ResponseEntity<FriendyResponse<Void>> withdrawal(
+        HttpServletRequest httpServletRequest
     ) {
         final String accessToken = jwtTokenExtractor.extractAccessToken(httpServletRequest);
         authService.withdrawal(accessToken);
 
-        return ResponseEntity.ok()
-                .build();
+        FriendyResponse<Void> response = FriendyResponse.of(AuthSuccessCode.MEMBER_WITHDRAWAL_SUCCESS);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/friendy/community/domain/auth/controller/code/AuthExceptionCode.java
+++ src/main/java/friendy/community/domain/auth/controller/code/AuthExceptionCode.java
@@ -1,4 +1,27 @@
 package friendy.community.domain.auth.controller.code;
 
-public enum AuthExceptionCode {
+import friendy.community.global.exception.dto.ExceptionCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AuthExceptionCode implements ExceptionCode {
+
+    LOGIN_FAILED_EXCEPTION(4101, "로그인에 실패하였습니다."),
+    ACCESS_TOKEN_EXTRACTION_FAILED(4102, "access 토큰 추출 실패"),
+    REFRESH_TOKEN_EXTRACTION_FAILED(4103, "refresh 토큰 추출 실패"),
+    ACCESS_TOKEN_EMAIL_MISSING(4104, "인증 실패(JWT 액세스 토큰 Payload 이메일 누락)"),
+    REFRESH_TOKEN_EMAIL_MISSING(4105, "인증 실패(JWT 리프레시 토큰 Payload 이메일 누락)"),
+    MALFORMED_ACCESS_TOKEN(4106, "잘못된 JWT 액세스 토큰 형식입니다."),
+    UNSUPPORTED_ACCESS_TOKEN(4107, "지원하지 않는 JWT 액세스 토큰입니다."),
+    EXPIRED_ACCESS_TOKEN(4108, "만료된 JWT 액세스 토큰입니다."),
+    EMPTY_ACCESS_TOKEN(4109, "JWT 액세스 토큰이 비어 있거나 잘못되었습니다."),
+    INVALID_ACCESS_TOKEN(4110, "JWT 액세스 토큰 검증 중 알 수 없는 오류가 발생했습니다."),
+    INVALID_REFRESH_TOKEN(4111, "잘못된 리프레시 토큰"),
+    EXPIRED_REFRESH_TOKEN(4112, "만료된 리프레시 토큰"),
+    USER_NOT_LOGGED_IN(4113, "사용자가 로그인되지 않았습니다.");
+
+    private final int code;
+    private final String message;
 }
