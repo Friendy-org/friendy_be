7f2de53dbe38ffc915e8a18c21092b90983edbd5
jw <rlawodnjs353@naver.com>
2025-03-24 17:31:39 +0900

    refactor(#86): follow 성공,예외 코드 변경

--- src/main/java/friendy/community/domain/follow/controller/FollowController.java
+++ src/main/java/friendy/community/domain/follow/controller/FollowController.java
@@ -1,5 +1,6 @@
 package friendy.community.domain.follow.controller;
 
+import friendy.community.domain.follow.controller.code.FollowSuccessCode;
 import friendy.community.domain.follow.dto.response.FollowListResponse;
 import friendy.community.domain.follow.service.FollowService;
 import friendy.community.global.response.FriendyResponse;
@@ -17,39 +18,37 @@ public class FollowController implements SpringDocFollowController {
 
     @PostMapping("/{targetId}")
     public ResponseEntity<FriendyResponse<Void>> follow(
-            @AuthenticationPrincipal FriendyUserDetails userDetails,
-            @PathVariable final Long targetId
+        @AuthenticationPrincipal FriendyUserDetails userDetails,
+        @PathVariable final Long targetId
     ) {
         followService.follow(userDetails.getMemberId(), targetId);
-        return ResponseEntity.ok(FriendyResponse.of(200, "팔로잉 성공"));
+        return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.FOLLOW_SUCCESS));
     }
 
     @DeleteMapping("/{targetId}")
     public ResponseEntity<FriendyResponse<Void>> unfollow(
-            @AuthenticationPrincipal FriendyUserDetails userDetails,
-            @PathVariable Long targetId
+        @AuthenticationPrincipal FriendyUserDetails userDetails,
+        @PathVariable Long targetId
     ) {
         followService.unfollow(userDetails.getMemberId(), targetId);
-        return ResponseEntity.ok(FriendyResponse.of(200, "언팔로우 성공"));
+        return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.UNFOLLOW_SUCCESS));
     }
 
     @GetMapping("/following/{targetId}")
     public ResponseEntity<FriendyResponse<FollowListResponse>> getFollowingMembers(
-            @PathVariable final Long targetId,
-            @RequestParam(required = false) Long lastFollowingId
+        @PathVariable final Long targetId,
+        @RequestParam(required = false) Long lastFollowingId
     ) {
-        return ResponseEntity.ok(FriendyResponse.of(200,
-                "팔로잉 목록 조회 성공",
-                followService.getFollowingMembers(targetId, lastFollowingId)));
+        return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.GET_FOLLOWING_LIST_SUCCESS,
+            followService.getFollowingMembers(targetId, lastFollowingId)));
     }
 
     @GetMapping("/follower/{targetId}")
     public ResponseEntity<FriendyResponse<FollowListResponse>> getFollowerMembers(
-            @PathVariable final Long targetId,
-            @RequestParam(required = false) Long lastFollowerId
+        @PathVariable final Long targetId,
+        @RequestParam(required = false) Long lastFollowerId
     ) {
-        return ResponseEntity.ok(FriendyResponse.of(200,
-                "팔로우 목록 조회 성공",
-                followService.getFollowerMembers(targetId, lastFollowerId)));
+        return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.GET_FOLLOWER_LIST_SUCCESS,
+            followService.getFollowerMembers(targetId, lastFollowerId)));
     }
 }
