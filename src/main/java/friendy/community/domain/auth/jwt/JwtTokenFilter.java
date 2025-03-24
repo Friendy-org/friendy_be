75f004905519ffdb6b02e50288361637b6211365
jw <rlawodnjs353@naver.com>
2025-03-24 17:32:44 +0900

    refactor(#86): token 예외 코드 변경

--- src/main/java/friendy/community/domain/auth/jwt/JwtTokenFilter.java
+++ src/main/java/friendy/community/domain/auth/jwt/JwtTokenFilter.java
@@ -1,7 +1,8 @@
 package friendy.community.domain.auth.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import friendy.community.global.exception.ErrorCode;
+import friendy.community.global.exception.domain.UnAuthorizedException;
+import friendy.community.global.exception.dto.ExceptionResponse;
 import friendy.community.global.security.FriendyUserDetails;
 import friendy.community.global.security.FriendyUserDetailsService;
 import jakarta.servlet.FilterChain;
@@ -46,7 +47,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             setAuthentication(token);
 
             filterChain.doFilter(request, response);
-        } catch (FriendyException e) {
+        } catch (UnAuthorizedException e) {
             log.error("JWT 검증 실패", e);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.setContentType("application/json");
@@ -54,7 +55,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
             ObjectMapper objectMapper = new ObjectMapper();
             String jsonResponse = objectMapper.writeValueAsString(
-                new ExceptionResponse(e.getErrorCode().getCode(), e.getMessage())
+                new ExceptionResponse(e.getExceptionType().getCode(), e.getMessage())
             );
             response.getWriter().write(jsonResponse);
         }
@@ -64,15 +65,15 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         String email;
         try {
             email = jwtTokenProvider.extractEmailFromAccessToken(token);
-        } catch (FriendyException e) {
-            throw new FriendyException(ErrorCode.UNAUTHORIZED_USER, "이메일 정보가 포함되지 않은 JWT 토큰입니다.");
+        } catch (UnAuthorizedException e) {
+            throw e;
         }
 
         FriendyUserDetails friendyUserDetails;
         try {
             friendyUserDetails = friendyUserDetailsService.loadUserByUsername(email);
         } catch (Exception e) {
-            throw new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "이메일 사용자가 없습니다.");
+            throw e;
         }
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(
