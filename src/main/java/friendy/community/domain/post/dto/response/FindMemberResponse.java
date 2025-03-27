010d5ab0efdd5f976b597d76fc00d83029611645
jw <rlawodnjs353@naver.com>
2025-03-27 10:09:42 +0900

    refactor(#86): profileImageUrl 추가

--- src/main/java/friendy/community/domain/post/dto/response/FindMemberResponse.java
+++ src/main/java/friendy/community/domain/post/dto/response/FindMemberResponse.java
@@ -3,10 +3,18 @@ package friendy.community.domain.post.dto.response;
 import friendy.community.domain.member.model.Member;
 
 public record FindMemberResponse(
-        Long id,
-        String nickname
+    Long id,
+    String nickname,
+    String profileImageUrl
 ) {
     public static FindMemberResponse from(Member member) {
-        return new FindMemberResponse(member.getId(), member.getNickname());
+        String profileImage;
+
+        if (member.getMemberImage() == null) {
+            profileImage = "defaultProfileImage";
+        } else {
+            profileImage = member.getMemberImage().getImageUrl();
+        }
+        return new FindMemberResponse(member.getId(), member.getNickname(), profileImage);
     }
 }
