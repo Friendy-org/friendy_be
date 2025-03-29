c0c344d43fa88766bef9aec3a9ae5bb8daea9aaf
wooki <hyunook02@naver.com>
2025-03-29 16:50:21 +0900

    feat(#80): 댓글 조회 성공 응답코드 작성

--- src/main/java/friendy/community/domain/comment/controller/code/CommentSuccessCode.java
+++ src/main/java/friendy/community/domain/comment/controller/code/CommentSuccessCode.java
@@ -11,7 +11,8 @@ public enum CommentSuccessCode implements ApiCode {
     COMMENT_CREATE_SUCCESS(1501, "댓글 작성 성공"),
     REPLY_CREATE_SUCCESS(1502, "답글 작성 성공"),
     COMMENT_UPDATE_SUCCESS(1503, "댓글 수정 성공"),
-    REPLY_UPDATE_SUCCESS(1504, "답글 수정 성공");
+    REPLY_UPDATE_SUCCESS(1504, "답글 수정 성공"),
+    GET_ALL_COMMENTS_SUCCESS(1505, "댓글 목록 조회 성공");
 
     private final int code;
     private final String message;
