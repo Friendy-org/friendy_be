cb819181746a8e9f7f03b1e91c57794944fa32e6
jw <rlawodnjs353@naver.com>
2025-04-01 10:42:43 +0900

    refactor(#88): 필요없는 path 제거

--- src/main/java/friendy/community/global/config/SecurityPathConfig.java
+++ src/main/java/friendy/community/global/config/SecurityPathConfig.java
@@ -1,8 +1,9 @@
 package friendy.community.global.config;
 
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SecurityPathConfig {
@@ -17,33 +18,7 @@ public class SecurityPathConfig {
         "/file"
     );
 
-    public static final List<String> PUBLIC_URIS = List.of(
-        "/follow/following",
-        "/follow/follower"
-    );
-
-//    public static final List<String> PUBLIC_API_URIS = List.of(
-//        "/login",
-//        "/register",
-//        "/posts/**",
-//        "/comments/**",
-//        "/public/images/**"
-//        // 필요에 따라 다른 경로들 추가
-//    );
-
-    public static final List<String> PUBLIC_GET_API_URIS = List.of(
-        "/posts/"
-    );
-
-    public static boolean isPublicApiUriWithMethod(final String path, final String method) {
-        if ("GET".equalsIgnoreCase(method)) {
-            return PUBLIC_GET_API_URIS.stream().anyMatch(path::startsWith);
-        }
-        return false;
-    }
-
     public static boolean isNoAuthUri(final String path) {
-        return PUBLIC_URIS.contains(path) ||
-            NO_AUTH_API_URIS.stream().anyMatch(path::startsWith);
+        return NO_AUTH_API_URIS.stream().anyMatch(path::startsWith);
     }
 }
