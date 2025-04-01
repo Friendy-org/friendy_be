fb40f4bef564e09b50e57035eb2aa7431bd184d4
jw <rlawodnjs353@naver.com>
2025-04-01 13:23:00 +0900

    refactor(#88): WebCorsConfig WebConfig로 통합

--- src/main/java/friendy/community/global/config/WebConfig.java
+++ src/main/java/friendy/community/global/config/WebConfig.java
@@ -2,25 +2,31 @@ package friendy.community.global.config;
 
 import friendy.community.global.security.FriendyUserDetailsService;
 import friendy.community.global.security.resolver.LoggedInUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
     private final FriendyUserDetailsService userDetailsService;
-
-    public WebConfig(FriendyUserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoggedInUserArgumentResolver(userDetailsService));
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600);
     }
 }
