636c3cc7a96c2774fcfd1c71fda63d0c030d8436
jw <rlawodnjs353@naver.com>
2025-04-03 16:26:46 +0900

    feat(#995): 내가 올린 게시글 response 구현

---
+++ src/main/java/friendy/community/domain/member/dto/response/PostPreview.java
@@ -0,0 +1,13 @@
+package friendy.community.domain.member.dto.response;
+
+import friendy.community.domain.post.model.Post;
+
+public record PostPreview(
+    Long postId,
+    String imageUrl
+) {
+    public static PostPreview from(Post post) {
+        String image = post.getImages().isEmpty() ? null : post.getImages().get(0).getImageUrl();
+        return new PostPreview(post.getId(), image);
+    }
+}
