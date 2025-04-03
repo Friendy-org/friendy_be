66ee261b008484e03ea0d9a69c2b80d5504c2e13
jw <rlawodnjs353@naver.com>
2025-04-03 17:49:16 +0900

    feat(#95): 프로필 게시글 조회 컨트롤러 추가

--- src/main/java/friendy/community/domain/member/controller/SpringDocMemberController.java
+++ src/main/java/friendy/community/domain/member/controller/SpringDocMemberController.java
@@ -3,7 +3,9 @@ package friendy.community.domain.member.controller;
 import friendy.community.domain.member.dto.request.MemberSignUpRequest;
 import friendy.community.domain.member.dto.request.PasswordRequest;
 import friendy.community.domain.member.dto.response.FindMemberResponse;
+import friendy.community.domain.member.dto.response.PagedMemberPostsResponse;
 import friendy.community.global.response.FriendyResponse;
+import friendy.community.global.security.FriendyUserDetails;
 import friendy.community.global.swagger.error.ApiErrorResponse;
 import friendy.community.global.swagger.error.ErrorCase;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "회원 API", description = "회원 API")
@@ -53,8 +56,22 @@ public interface SpringDocMemberController {
             @ErrorCase(description = "존재하지 않는 회원", exampleMessage = "존재하지 않는 회원입니다.")
     })
     ResponseEntity<FriendyResponse<FindMemberResponse>> getMemberInfo(
-            HttpServletRequest httpServletRequest,
+            @AuthenticationPrincipal FriendyUserDetails userDetails,
             @PathVariable Long memberId
+    );
+
+    @Operation(summary = "회원 게시글 목록 조회")
+    @ApiResponse(responseCode = "200", description = "회원 게시글 목록 조회 성공")
+    @ApiErrorResponse(
+        status = HttpStatus.NOT_FOUND,
+        instance = "/member/{memberId}/posts",
+        errorCases = {
+            @ErrorCase(description = "존재하지 않는 회원", exampleMessage = "존재하지 않는 회원입니다.")
+        }
+    )
+    ResponseEntity<FriendyResponse<PagedMemberPostsResponse>> getMemberPosts(
+        @AuthenticationPrincipal FriendyUserDetails userDetails,
+        @PathVariable Long memberId
     );
 
 }
