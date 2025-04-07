aed8748765b42713e4f3b42b29298eccdfae15ca
jw <rlawodnjs353@naver.com>
2025-04-07 10:41:02 +0900

    feat(#95): 성공코드 추가

--- src/main/java/friendy/community/domain/post/controller/code/PostSuccessCode.java
+++ src/main/java/friendy/community/domain/post/controller/code/PostSuccessCode.java
@@ -12,7 +12,8 @@ public enum PostSuccessCode implements ApiCode {
     UPDATE_POST_SUCCESS(1402, "게시글 수정 성공"),
     DELETE_POST_SUCCESS(1403, "게시글 삭제 성공"),
     GET_POST_SUCCESS(1404, "게시글 조회 성공"),
-    GET_ALL_POSTS_SUCCESS(1405, "게시글 목록 조회 성공");
+    GET_ALL_POSTS_SUCCESS(1405, "게시글 목록 조회 성공"),
+    GET_MEMBER_POSTS_SUCCESS(1406, "특정 멤버 게시글 목록 조회 성공");
 
     private final int code;
     private final String message;
