a26197f18abb704a289b8999b8b3e1be29af63a5
jw <rlawodnjs353@naver.com>
2025-04-11 13:04:59 +0900

    refactor(#95): 예외코드 변경

--- src/main/java/friendy/community/domain/follow/controller/code/FollowExceptionCode.java
+++ src/main/java/friendy/community/domain/follow/controller/code/FollowExceptionCode.java
@@ -10,10 +10,9 @@ public enum FollowExceptionCode implements ExceptionCode {
 
     ALREADY_FOLLOWED(4601, "이미 팔로우한 회원입니다."),
     NOT_FOLLOWED(4602, "팔로우하지 않은 회원입니다."),
-    MEMBER_NOT_FOUND(4603, "해당 ID의 회원을 찾을 수 없습니다."),
-    SELF_FOLLOW_NOT_ALLOWED(4604, "자기 자신을 대상으로 수행할 수 없습니다."),
-    FOLLOWING_MEMBER_NOT_FOUND(4605, "팔로잉 멤버가 없습니다."),
-    FOLLOWER_MEMBER_NOT_FOUND(4606, "팔로워 멤버가 없습니다.");
+    SELF_FOLLOW_NOT_ALLOWED(4603, "자기 자신을 대상으로 수행할 수 없습니다."),
+    FOLLOWING_MEMBER_NOT_FOUND(4604, "팔로잉 멤버가 없습니다."),
+    FOLLOWER_MEMBER_NOT_FOUND(4605, "팔로워 멤버가 없습니다.");
 
     private final int code;
     private final String message;
