5a3803959e1fa07a9bca7d20a5dc5b83667e30ed
jw <rlawodnjs353@naver.com>
2025-04-11 13:10:21 +0900

    refactor(#95): Post 리포지토리 분리

--- src/main/java/friendy/community/domain/post/repository/PostRepository.java
+++ src/main/java/friendy/community/domain/post/repository/PostRepository.java
@@ -1,9 +1,10 @@
 package friendy.community.domain.post.repository;
 
 import friendy.community.domain.post.model.Post;
+import friendy.community.domain.post.repository.query.PostQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
 }
