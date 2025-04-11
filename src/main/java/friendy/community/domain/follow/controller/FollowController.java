412764db5e4da9fb861e536c6b15bb6194fb6693
jw <rlawodnjs353@naver.com>
2025-04-11 13:04:41 +0900

    refactor(#95): 팔로우 서비스 분리로 인한 리팩토링

--- src/main/java/friendy/community/domain/follow/controller/FollowController.java
+++ src/main/java/friendy/community/domain/follow/controller/FollowController.java
@@ -2,27 +2,29 @@ package friendy.community.domain.follow.controller;
 
 import friendy.community.domain.follow.controller.code.FollowSuccessCode;
 import friendy.community.domain.follow.dto.response.FollowListResponse;
-import friendy.community.domain.follow.service.FollowService;
+import friendy.community.domain.follow.service.FollowCommandService;
+import friendy.community.domain.follow.service.FollowQueryService;
 import friendy.community.global.response.FriendyResponse;
 import friendy.community.global.security.FriendyUserDetails;
 import friendy.community.global.security.annotation.LoggedInUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/follow")
 public class FollowController implements SpringDocFollowController {
-    private final FollowService followService;
+
+    private final FollowCommandService followCommandService;
+    private final FollowQueryService followQueryService;
 
     @PostMapping("/{targetId}")
     public ResponseEntity<FriendyResponse<Void>> follow(
         @LoggedInUser FriendyUserDetails userDetails,
         @PathVariable final Long targetId
     ) {
-        followService.follow(userDetails.getMemberId(), targetId);
+        followCommandService.follow(userDetails.getMemberId(), targetId);
         return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.FOLLOW_SUCCESS));
     }
 
@@ -31,7 +33,7 @@ public class FollowController implements SpringDocFollowController {
         @LoggedInUser FriendyUserDetails userDetails,
         @PathVariable Long targetId
     ) {
-        followService.unfollow(userDetails.getMemberId(), targetId);
+        followCommandService.unfollow(userDetails.getMemberId(), targetId);
         return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.UNFOLLOW_SUCCESS));
     }
 
@@ -41,7 +43,7 @@ public class FollowController implements SpringDocFollowController {
         @RequestParam(required = false) Long lastFollowingId
     ) {
         return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.GET_FOLLOWING_LIST_SUCCESS,
-            followService.getFollowingMembers(targetId, lastFollowingId)));
+            followQueryService.getFollowingList(targetId, lastFollowingId)));
     }
 
     @GetMapping("/follower/{targetId}")
@@ -50,6 +52,6 @@ public class FollowController implements SpringDocFollowController {
         @RequestParam(required = false) Long lastFollowerId
     ) {
         return ResponseEntity.ok(FriendyResponse.of(FollowSuccessCode.GET_FOLLOWER_LIST_SUCCESS,
-            followService.getFollowerMembers(targetId, lastFollowerId)));
+            followQueryService.getFollowerList(targetId, lastFollowerId)));
     }
 }
