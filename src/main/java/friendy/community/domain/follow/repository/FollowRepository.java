ca365530e5793315fa2ada9c88b595171eaa0164
jw <rlawodnjs353@naver.com>
2025-04-11 13:04:06 +0900

    refactor(#95): 팔로우 리포지토리 분리

--- src/main/java/friendy/community/domain/follow/repository/FollowRepository.java
+++ src/main/java/friendy/community/domain/follow/repository/FollowRepository.java
@@ -1,19 +1,18 @@
 package friendy.community.domain.follow.repository;
 
 import friendy.community.domain.follow.model.Follow;
+import friendy.community.domain.follow.repository.query.FollowQueryRepository;
 import friendy.community.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface FollowRepository extends JpaRepository<Follow, Long> {
+public interface FollowRepository extends JpaRepository<Follow, Long>, FollowQueryRepository {
     boolean existsByFollowerAndFollowing(Member follower, Member following);
 
-    Optional<Follow> findByFollowerAndFollowing(Member follower, Member following);
+    Follow findByFollowerAndFollowing(Member follower, Member following);
 
-    long countByFollowing(Member following);
+    int countByFollowing(Member following);
 
-    long countByFollower(Member follower);
+    int countByFollower(Member follower);
 }
